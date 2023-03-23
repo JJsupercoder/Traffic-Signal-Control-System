715297363a1d614e6ef763bd20cd5b2a99706dd5
Nirav Gujarathi <82361028+Netizen02@users.noreply.github.com>
2023-03-23 16:01:33 +0530

    Update Virtual_Environment.py

--- Virtual_Environment.py
+++ Virtual_Environment.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -18,46 +19,55 @@ YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
 
 # define road dimensions
-ROAD_WIDTH = 60
+ROAD_WIDTH = WIDTH*0.075
 ROAD_LENGTH = 300
 INTERSECTION_SIZE = 100
 
-# create roads
-road1 = pygame.Rect((WIDTH - ROAD_WIDTH) / 2, 0, ROAD_WIDTH, ROAD_LENGTH)
-road2 = pygame.Rect((WIDTH - ROAD_WIDTH) / 2, HEIGHT - ROAD_LENGTH, ROAD_WIDTH, ROAD_LENGTH)
-road3 = pygame.Rect(0, (HEIGHT - ROAD_WIDTH) / 2, ROAD_LENGTH, ROAD_WIDTH)
-road4 = pygame.Rect(WIDTH - ROAD_LENGTH, (HEIGHT - ROAD_WIDTH) / 2, ROAD_LENGTH, ROAD_WIDTH)
-road5 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2, (HEIGHT - INTERSECTION_SIZE) / 2, INTERSECTION_SIZE, INTERSECTION_SIZE)
+#create dividers: go to line 41
 
-# create intersections
-intersection1 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2, 0, INTERSECTION_SIZE, INTERSECTION_SIZE)
-intersection2 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2, HEIGHT - INTERSECTION_SIZE, INTERSECTION_SIZE, INTERSECTION_SIZE)
-intersection3 = pygame.Rect(0, (HEIGHT - INTERSECTION_SIZE) / 2, INTERSECTION_SIZE, INTERSECTION_SIZE)
-intersection4 = pygame.Rect(WIDTH - INTERSECTION_SIZE, (HEIGHT - INTERSECTION_SIZE) / 2, INTERSECTION_SIZE, INTERSECTION_SIZE)
+# create traffic signals: go to line 
 
-# create traffic signals
-signal1 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2, (HEIGHT - INTERSECTION_SIZE) / 2 - 20, 20, 20)
-signal2 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2 + INTERSECTION_SIZE, (HEIGHT - INTERSECTION_SIZE) / 2, 20, 20)
-signal3 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2, (HEIGHT - INTERSECTION_SIZE) / 2 + INTERSECTION_SIZE, 20, 20)
-signal4 = pygame.Rect((WIDTH - INTERSECTION_SIZE) / 2 - 20, (HEIGHT - INTERSECTION_SIZE) / 2, 20, 20)
+# create obstacles
+obstacle1 = pygame.Rect((ROAD_WIDTH), (ROAD_WIDTH), (WIDTH - (3*ROAD_WIDTH))/2,(HEIGHT - (3*ROAD_WIDTH))/2)
+obstacle2 = pygame.Rect(((ROAD_WIDTH + WIDTH)/2), (ROAD_WIDTH), (WIDTH - (3*ROAD_WIDTH))/2, (HEIGHT - (3*ROAD_WIDTH))/2)
+obstacle3 = pygame.Rect((ROAD_WIDTH), ((ROAD_WIDTH + HEIGHT)/2), (WIDTH - (3*ROAD_WIDTH))/2,(HEIGHT - (3*ROAD_WIDTH))/2)
+obstacle4 = pygame.Rect(((ROAD_WIDTH + WIDTH)/2), ((ROAD_WIDTH + HEIGHT)/2), (WIDTH - (3*ROAD_WIDTH))/2,(HEIGHT - (3*ROAD_WIDTH))/2)
+
 
 # draw roads, intersections and signals
 def draw_environment():
     win.fill(GREY)
-    pygame.draw.rect(win, WHITE, road1)
-    pygame.draw.rect(win, WHITE, road2)
-    pygame.draw.rect(win, WHITE, road3)
-    pygame.draw.rect(win, WHITE, road4)
-    pygame.draw.rect(win, WHITE, road5)
-    pygame.draw.rect(win, GREY, intersection1)
-    pygame.draw.rect(win, GREY, intersection2)
-    pygame.draw.rect(win, GREY, intersection3)
-    pygame.draw.rect(win, GREY, intersection4)
-    pygame.draw.rect(win, RED, signal1)
-    pygame.draw.rect(win, GREEN, signal2)
-    pygame.draw.rect(win, RED, signal3)
-    pygame.draw.rect(win, GREEN, signal4)
+    # create dividers
+    pygame.draw.line(win, WHITE, ((ROAD_WIDTH/2), (ROAD_WIDTH/2)), (((WIDTH - ROAD_WIDTH)/2),(ROAD_WIDTH/2)))
+    pygame.draw.line(win, WHITE, (((ROAD_WIDTH + WIDTH)/2),(ROAD_WIDTH/2)), ((WIDTH - (ROAD_WIDTH/2)),(ROAD_WIDTH/2)))
+    pygame.draw.line(win, WHITE, ((WIDTH - (ROAD_WIDTH/2)),(ROAD_WIDTH/2)), ((WIDTH - (ROAD_WIDTH/2)), (HEIGHT-ROAD_WIDTH)/2))
+    pygame.draw.line(win, WHITE, ((WIDTH - (ROAD_WIDTH/2)),(HEIGHT+ROAD_WIDTH)/2), ((WIDTH - (ROAD_WIDTH/2)),(HEIGHT-(ROAD_WIDTH/2))))
+    pygame.draw.line(win, WHITE, ((WIDTH - (ROAD_WIDTH/2)),(HEIGHT - (ROAD_WIDTH/2))), (((ROAD_WIDTH + WIDTH)/2),(HEIGHT - (ROAD_WIDTH/2))))
+    pygame.draw.line(win, WHITE, (((WIDTH - ROAD_WIDTH)/2),(HEIGHT - (ROAD_WIDTH/2))), ((ROAD_WIDTH/2),(HEIGHT - (ROAD_WIDTH/2))))
+    pygame.draw.line(win, WHITE, ((ROAD_WIDTH/2),(HEIGHT-(ROAD_WIDTH/2))), ((ROAD_WIDTH/2),(HEIGHT+ROAD_WIDTH)/2))
+    pygame.draw.line(win, WHITE, ((ROAD_WIDTH/2),(HEIGHT-ROAD_WIDTH)/2), ((ROAD_WIDTH/2),(ROAD_WIDTH/2)))
+    pygame.draw.line(win, WHITE, ((WIDTH/2),(ROAD_WIDTH)), ((WIDTH/2),((HEIGHT-ROAD_WIDTH)/2)))
+    pygame.draw.line(win, WHITE, ((ROAD_WIDTH),(HEIGHT/2)), (((WIDTH-ROAD_WIDTH)/2),(HEIGHT/2)))
+    pygame.draw.line(win, WHITE, ((WIDTH/2),((HEIGHT+ROAD_WIDTH)/2)), ((WIDTH/2),(HEIGHT-ROAD_WIDTH)))
+    pygame.draw.line(win, WHITE, (((WIDTH+ROAD_WIDTH)/2),(HEIGHT/2)), ((WIDTH-ROAD_WIDTH),(HEIGHT/2)))
+    # draw obstacles
+    pygame.draw.rect(win,GREEN, obstacle1)
+    pygame.draw.rect(win,GREEN, obstacle2)
+    pygame.draw.rect(win,GREEN, obstacle3)
+    pygame.draw.rect(win,GREEN, obstacle4)
+    # create traffic signals
     pygame.display.update()
 
 # game loop
-run = True
+# gameover = False
+
+# while not gameover:
+#     pygame.
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+    draw_environment()
