90a5d5bb47a297f4336a00537419cc39d79b9a19
Yashdeshapp01 <118040942+Yashdeshapp01@users.noreply.github.com>
2023-03-23 16:53:38 +0530

    Update Virtual_Environment.py

--- Virtual_Environment.py
+++ Virtual_Environment.py
@@ -26,6 +26,24 @@ INTERSECTION_SIZE = 100
 #create dividers: go to line 41
 
 # create traffic signals: go to line 
+# Traffic Signal Colour
+s1 = RED
+s2 = RED
+s3 = RED
+s4 = RED
+s5 = RED
+s6 = RED
+s7 = RED
+s8 = RED
+s9 = RED
+s10 = RED
+s11 = RED
+s12 = RED
+s13 = RED
+s14 = RED
+s15 = RED
+s16 = RED
+
 
 # create obstacles
 obstacle1 = pygame.Rect((ROAD_WIDTH), (ROAD_WIDTH), (WIDTH - (3*ROAD_WIDTH))/2,(HEIGHT - (3*ROAD_WIDTH))/2)
@@ -56,6 +74,27 @@ def draw_environment():
     pygame.draw.rect(win,GREEN, obstacle3)
     pygame.draw.rect(win,GREEN, obstacle4)
     # create traffic signals
+    pygame.draw.circle(win,s1,((WIDTH/2-0.75*ROAD_WIDTH),(ROAD_WIDTH/4)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s2,((WIDTH/2+0.75*ROAD_WIDTH),(ROAD_WIDTH*0.75)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s3,((WIDTH/2-0.25*ROAD_WIDTH),(5*ROAD_WIDTH/4)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s4,((WIDTH-0.25*ROAD_WIDTH),(HEIGHT/2-0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH-1.25*ROAD_WIDTH),(HEIGHT/2-0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH-0.75*ROAD_WIDTH),(HEIGHT/2+0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH/2+0.75*ROAD_WIDTH),(HEIGHT-0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH/2+0.25*ROAD_WIDTH),(HEIGHT-1.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH/2-0.75*ROAD_WIDTH),(HEIGHT-0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH-1.25*ROAD_WIDTH),(HEIGHT/2-0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH-1.25*ROAD_WIDTH),(HEIGHT/2-0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s1,((WIDTH-1.25*ROAD_WIDTH),(HEIGHT/2-0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s10,((ROAD_WIDTH*0.25),(HEIGHT/2+0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s11,((ROAD_WIDTH*0.75),(HEIGHT/2-0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s12,((ROAD_WIDTH*1.25),(HEIGHT/2+0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s15,((WIDTH/2-0.25*ROAD_WIDTH),(HEIGHT/2+0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))    
+    pygame.draw.circle(win,s13,((WIDTH/2-0.75*ROAD_WIDTH),((HEIGHT/2) - (ROAD_WIDTH/4))),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s12,((WIDTH/2+0.75*ROAD_WIDTH),(HEIGHT/2+0.25*ROAD_WIDTH)),(ROAD_WIDTH/4))
+    pygame.draw.circle(win,s16,((WIDTH/2+0.25*ROAD_WIDTH),(HEIGHT/2-0.75*ROAD_WIDTH)),(ROAD_WIDTH/4))
+
+
     pygame.display.update()
 
 # game loop
